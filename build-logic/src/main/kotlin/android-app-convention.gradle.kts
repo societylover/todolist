3f784e6d1959375357cd5b54ecd008f216b8b0ac
ekuznetsov <ohhgodsave@gmail.com>
2024-07-13 04:58:02 +0300

    Update build config

--- build-logic/src/main/kotlin/android-app-convention.gradle.kts
+++ build-logic/src/main/kotlin/android-app-convention.gradle.kts
@@ -15,16 +15,9 @@ configure<BaseAppModuleExtension> {
         kotlinCompilerExtensionVersion = "1.5.8"
     }
 
-    val clientId: String by project
-    val baseUrl: String by project
-    val appStoreFile: String by project
-    val appStorePassword: String by project
-    val appKeyAlias: String by project
-    val appKeyPassword: String by project
-
     defaultConfig {
-        buildConfigField("String", "BASE_URL", baseUrl)
-        manifestPlaceholders["YANDEX_CLIENT_ID"] = clientId
+        buildConfigField("String", "BASE_URL", providers.environmentVariable("BASE_URL").toString())
+        manifestPlaceholders["YANDEX_CLIENT_ID"] = providers.environmentVariable("CLIENT_ID")
     }
 }
 

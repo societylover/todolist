76027a4f54b35507f408e979178bca691a07cba0
ekuznetsov <ohhgodsave@gmail.com>
2024-07-27 01:10:53 +0300

    Remove tgreporter usage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.serialization) version libs.versions.kotlin
     kotlin("kapt")
-    id("telegram-reporter")
     alias(libs.plugins.org.jetbrains.kotlin.android)
 }
 
